7d6d5414fd88a9099060d64d3f7d48b9063406e2
hooni <jhlee909090@gmail.com>
2024-12-26 23:29:55 +0900

    add: xcode 연결

---
+++ SpoonMe/SpoonMe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SpoonMe
+//
+//  Created by 이지훈 on 12/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SpoonMe/SpoonMe/SpoonMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpoonMeApp.swift
+//  SpoonMe
+//
+//  Created by 이지훈 on 12/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SpoonMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
