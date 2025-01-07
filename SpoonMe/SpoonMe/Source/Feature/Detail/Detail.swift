512f8f330d6490cae0a90242bff6760ac99ea1f7
JURI <80569323+juri123123@users.noreply.github.com>
2025-01-07 13:31:19 +0900

    chore: #16 tabView에 넣을 view로 파일 변경

--- SpoonMe/SpoonMe/Source/Feature/Detail/Detail.swift
+++ SpoonMe/SpoonMe/Source/Feature/Detail/Detail.swift
@@ -5,4 +5,10 @@
 //  Created by 이지훈 on 1/2/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct Detail: View {
+    var body: some View {
+        Text("Detail")
+    }
+}
--- SpoonMe/SpoonMe/Source/Feature/Home/Home.swift
+++ SpoonMe/SpoonMe/Source/Feature/Home/Home.swift
@@ -5,4 +5,10 @@
 //  Created by 이지훈 on 1/2/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Home")
+    }
+}
