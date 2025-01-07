d8b577d0e79d0c046e9bb6611770417f112115dd
JURI <80569323+juri123123@users.noreply.github.com>
2025-01-07 13:50:58 +0900

    feat: #16 네비게이션 관리 class 생성, view enum 파일 생성

---
+++ SpoonMe/SpoonMe/Resource/Tab/NavigationManager.swift
@@ -0,0 +1,60 @@
+//
+//  NavigationManager.swift
+//  SpoonMe
+//
+//  Created by 최주리 on 1/7/25.
+//
+
+import SwiftUI
+
+final class NavigationManager: ObservableObject {
+    
+    @Published var selectedTab: TabViewType = .map
+    
+    @Published var mapPath: [ViewType] = []
+    @Published var explorePath: [ViewType] = []
+    @Published var registerPath: [ViewType] = []
+    
+    @ViewBuilder
+    func build(_ view: ViewType) -> some View {
+        switch view {
+            // 임시
+        case .test:
+            Quest()
+        }
+    }
+    
+    func push(_ view: ViewType) {
+        switch selectedTab {
+        case .map:
+            mapPath.append(view)
+        case .explore:
+            explorePath.append(view)
+        case .register:
+            registerPath.append(view)
+        }
+    }
+    
+    func pop(_ depth: Int) {
+        switch selectedTab {
+        case .map:
+            mapPath.removeLast(depth)
+        case .explore:
+            explorePath.removeLast(depth)
+        case .register:
+            registerPath.removeLast(depth)
+        }
+    }
+    
+    func popToRoot() {
+        switch selectedTab {
+        case .map:
+            mapPath = []
+        case .explore:
+            explorePath = []
+        case .register:
+            registerPath = []
+        }
+    }
+    
+}
