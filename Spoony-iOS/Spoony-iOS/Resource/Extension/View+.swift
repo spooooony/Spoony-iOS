d94036653b8dbef80e170554f7874c50a7c853d3
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-01-12 01:25:06 +0900

    feat: #21 ToastModifier 구현

--- Spoony-iOS/Spoony-iOS/Resource/Extension/View+.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Extension/View+.swift
@@ -5,4 +5,10 @@
 //  Created by 이지훈 on 1/2/25.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    func toastView(toast: Binding<Toast?>) -> some View {
+        self.modifier(ToastModifier(toast: toast))
+    }
+}
--- Spoony-iOS/Spoony-iOS/Resource/Helper/ToastModifier.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Helper/ToastModifier.swift
@@ -24,7 +24,7 @@ extension ToastStyle {
     var themeColor: Color {
         switch self {
         case .grey: return .gray600
-}
+        }
     }
 }
 
@@ -46,4 +46,70 @@ struct ToastView: View {
         .background(style.themeColor)
         .cornerRadius(8)
     }
+}
+
+struct ToastModifier: ViewModifier {
+    
+    @Binding var toast: Toast?
+    @State private var workItem: DispatchWorkItem?
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay(
+                ZStack {
+                    mainToastView()
+                }.animation(.spring(), value: toast)
+            )
+            .onChange(of: toast, initial: false) {
+                showToast()
+            }
+    }
+    
+    @ViewBuilder
+    func mainToastView() -> some View {
+        if let toast = toast {
+            VStack {
+                Spacer().frame(height: toast.yOffset)
+                ToastView(
+                    style: toast.style,
+                    message: toast.message,
+                    width: toast.width
+                ) {
+                    dismissToast()
+                }
+            }
+        }
+    }
+    
+    private func showToast() {
+        guard let toast = toast else { return }
+        
+        UIImpactFeedbackGenerator(style: .light)
+            .impactOccurred()
+        
+        if toast.duration > 0 {
+            workItem?.cancel()
+            
+            let task = DispatchWorkItem {
+                dismissToast()
+            }
+            
+            workItem = task
+            DispatchQueue.main.asyncAfter(deadline: .now() + toast.duration, execute: task)
+        }
+    }
+    
+    private func dismissToast() {
+        withAnimation {
+            toast = nil
+        }
+        
+        workItem?.cancel()
+        workItem = nil
+    }
+}
+
+#Preview {
+    ContentView()
 }
