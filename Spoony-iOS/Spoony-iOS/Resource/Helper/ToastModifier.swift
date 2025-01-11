4b015451be76cfad4d583d4507d7a4dfb097d996
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-01-12 01:24:22 +0900

    feat: #21 ToastView 구현

--- Spoony-iOS/Spoony-iOS/Resource/Helper/ToastModifier.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Helper/ToastModifier.swift
@@ -20,3 +20,30 @@ public enum ToastStyle {
     case grey
 }
 
+extension ToastStyle {
+    var themeColor: Color {
+        switch self {
+        case .grey: return .gray600
+}
+    }
+}
+
+struct ToastView: View {
+    
+    var style: ToastStyle
+    var message: String
+    var width = CGFloat.infinity
+    var onCancelTapped: (() -> Void)
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 12) {
+            Text(message)
+                .font(.body2b)
+                .foregroundColor(.white)
+        }
+        .padding()
+        .frame(minWidth: 0, maxWidth: width)
+        .background(style.themeColor)
+        .cornerRadius(8)
+    }
+}
