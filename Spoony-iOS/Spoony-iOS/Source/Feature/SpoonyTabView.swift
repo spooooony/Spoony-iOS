7688a4a74cdd6fb31266ed8d39e76fc5a316284d
ChoiAnYong <dksdyd78@naver.com>
2025-01-13 01:31:19 +0900

    style: #28 TabView 디자인 반영

--- Spoony-iOS/Spoony-iOS/Source/Feature/SpoonyTabView.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/SpoonyTabView.swift
@@ -11,37 +11,50 @@ struct SpoonyTabView: View {
     
     @EnvironmentObject var navigationManager: NavigationManager
     
+    init() {
+        UITabBar.appearance().scrollEdgeAppearance = .init()
+        UITabBar.appearance().scrollEdgeAppearance?.backgroundColor = .white
+    }
+    
     var body: some View {
         
         TabView(selection: $navigationManager.selectedTab) {
-            NavigationStack(path: $navigationManager.mapPath) {
-                Home()
-                    .navigationDestination(for: ViewType.self) { view in
-                        navigationManager.build(view)
+            ForEach(TabType.allCases, id: \.self) { tab in
+                Group {
+                    switch tab {
+                    case .map:
+                        NavigationStack(path: $navigationManager.mapPath) {
+                            Home()
+                                .navigationDestination(for: ViewType.self) { view in
+                                    navigationManager.build(view)
+                                }
+                        }
+                    case .explore:
+                        NavigationStack(path: $navigationManager.explorePath) {
+                            Detail()
+                                .navigationDestination(for: ViewType.self) { view in
+                                    navigationManager.build(view)
+                                }
+                        }
+                    case .register:
+                        NavigationStack(path: $navigationManager.registerPath) {
+                            Register()
+                                .navigationDestination(for: ViewType.self) { view in
+                                    navigationManager.build(view)
+                                }
+                        }
                     }
+                }
+                .tabItem {
+                    Label(
+                        tab.title,
+                        image: tab.imageName(selected: navigationManager.selectedTab == tab)
+                    )
+                }
+                .tag(tab)
             }
-            .tabItem { Label("내 지도", systemImage: "safari") }
-            .tag(TabType.map)
-            
-            NavigationStack(path: $navigationManager.explorePath) {
-                Detail()
-                    .navigationDestination(for: ViewType.self) { view in
-                        navigationManager.build(view)
-                    }
-            }
-            .tabItem { Label("탐색", systemImage: "magnifyingglass") }
-            .tag(TabType.explore)
-            
-            NavigationStack(path: $navigationManager.registerPath) {
-                Register()
-                    .navigationDestination(for: ViewType.self) { view in
-                        navigationManager.build(view)
-                    }
-            }
-            .tabItem { Label("등록", systemImage: "plus.circle") }
-            .tag(TabType.register)
         }
-        
+        .accentColor(.main400)
     }
 }
 
