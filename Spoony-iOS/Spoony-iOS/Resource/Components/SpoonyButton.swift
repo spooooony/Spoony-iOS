77b0c5978652236ad3f9b3d38e7346d2fccd2689
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-01-12 16:06:05 +0900

    style: #23 뷰 마다 다르게 사용 될 수 있는 SpoonyButtonStyle 정의

--- Spoony-iOS/Spoony-iOS/Resource/Components/SpoonyButton.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Components/SpoonyButton.swift
@@ -57,3 +57,52 @@ public enum SpoonyButtonSize {
         }
     }
 }
+
+@frozen
+public enum SpoonyButtonStyle {
+    case primary
+    case secondary
+    case teritary
+    
+    var enabledColor: Color {
+        switch self {
+        case .primary:
+            return .main400
+        case .secondary:
+            return .spoonBlack
+        case .teritary:
+            return .gray0
+        }
+    }
+    
+    var pressedColor: Color {
+        switch self {
+        case .primary:
+            return .main500
+        case .secondary:
+            return .gray800
+        case .teritary:
+            return .gray100
+        }
+    }
+    
+    var disabledColor: Color {
+        switch self {
+        case .primary:
+            return .main100
+        case .secondary:
+            return .gray300
+        case .teritary:
+            return .gray100
+        }
+    }
+    
+    var foregroundColor: Color {
+        switch self {
+        case .primary, .secondary:
+            return .white
+        case .teritary:
+            return .gray600
+        }
+    }
+}
