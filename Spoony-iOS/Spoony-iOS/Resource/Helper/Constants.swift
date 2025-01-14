238fa8c5a1678b041fd2566f78ab227d9220b78f
hooni <jhlee909090@gmail.com>
2025-01-14 13:04:30 +0900

    feat: #32 baseURL 추가

--- Spoony-iOS/Spoony-iOS/Resource/Helper/Constants.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Helper/Constants.swift
@@ -9,6 +9,15 @@ import Foundation
 
 struct Constants {
     
+    struct API {
+            static var baseURL: String {
+                guard let url = Bundle.main.object(forInfoDictionaryKey: "BASE_URL") as? String else {
+                    fatalError("BASE_URL not found in Info.plist")
+                }
+                return url
+            }
+        }
+    
     struct NaverMap {
         static var clientId: String {
             guard let clientId = Bundle.main.object(forInfoDictionaryKey: "NMFClientId") as? String else {
