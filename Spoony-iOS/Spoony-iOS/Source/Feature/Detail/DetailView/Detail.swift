4c2c288963ef53848a4be0929869fd1707baf108
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-01-15 13:50:47 +0900

    feat: #46 - dash view에 필요한 Line 구조체 구현

--- Spoony-iOS/Spoony-iOS/Source/Feature/Detail/DetailView/Detail.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Detail/DetailView/Detail.swift
@@ -30,3 +30,12 @@ struct Detail: View {
 #Preview {
     Detail()
 }
+
+struct Line: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        path.move(to: CGPoint(x: 0, y: 0))
+        path.addLine(to: CGPoint(x: rect.width, y: 0))
+        return path
+    }
+    }
