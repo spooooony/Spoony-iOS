06f2cfb35679fce2b11628cedc2fbcbde4386dc7
hooni <jhlee909090@gmail.com>
2025-01-16 09:52:22 +0900

    style: #60 엠티뷰 구현

--- Spoony-iOS/Spoony-iOS/Source/Feature/Home/Search/SearchViewTest.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Home/Search/SearchViewTest.swift
@@ -8,24 +8,37 @@
 import SwiftUI
 
 struct SearchViewTest: View {
-   @State private var searchText = ""
-   
-   var body: some View {
-       VStack(spacing: 0) {
-           CustomNavigationBar(
-               style: .searchBar,
-               searchText: $searchText,
-               onBackTapped: {},
-               onSearchSubmit: {
-                   print("Search submitted: \(searchText)")
-               }
-           )
-           
-           Spacer()
-       }
-   }
+    @State private var searchText = ""
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                CustomNavigationBar(
+                    style: .searchBar,
+                    searchText: $searchText,
+                    onBackTapped: {},
+                    onSearchSubmit: {
+                        print("Search submitted: \(searchText)")
+                    }
+                )
+                
+                Spacer()
+            }
+            VStack(spacing: 8) {
+                Rectangle()
+                    .fill(Color(.gray200))
+                    .frame(width: 200, height: 200)
+                    .padding(.bottom, 12)
+                
+                Text("구체적인 장소를 검색해 보세요")
+                    .font(.system(size: 16))
+                    .foregroundColor(Color(.gray600))
+            }
+            .offset(y: -100)
+        }
+    }
 }
 
 #Preview {
-   SearchViewTest()
+    SearchViewTest()
 }
