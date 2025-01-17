834fe187cef01e772caaaf661e0f1aa765790be6
ChoiAnYong <dksdyd78@naver.com>
2025-01-17 15:44:00 +0900

    feat: #51 RegisterStore 구현

--- Spoony-iOS/Spoony-iOS/Source/Feature/Register/State/RegisterStore.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Register/State/RegisterStore.swift
@@ -9,5 +9,34 @@ import Foundation
 
 final class RegisterStore: ObservableObject {
     @Published var step: RegisterStep = .start
+    @Published var disableFirstButton: Bool = true
     @Published var text: String = ""
+    @Published var isSelected: Bool = false
+    @Published var categorys: [CategoryChip] = CategoryChip.sample()
+    @Published var recommendMenu: [String] = [""] {
+        didSet {
+            firstButtonInvalid()
+        }
+    }
+    @Published var selectedCategory: [CategoryChip] = [] {
+        didSet {
+            firstButtonInvalid()
+        }
+    }
+    @Published var searchPlaces: [PlaceInfo] = PlaceInfo.sample()
+    @Published var selectedPlace: PlaceInfo? {
+        didSet {
+            firstButtonInvalid()
+        }
+    }
+    
+    func firstButtonInvalid() {
+        let isRecommendMenuInvalid = recommendMenu.contains { $0.isEmpty }
+        
+        let isSelectedCategoryInvalid = selectedCategory.isEmpty
+
+        let isSelectedPlaceInvalid = selectedPlace == nil
+
+        disableFirstButton = isRecommendMenuInvalid || isSelectedCategoryInvalid || isSelectedPlaceInvalid
+    }
 }
