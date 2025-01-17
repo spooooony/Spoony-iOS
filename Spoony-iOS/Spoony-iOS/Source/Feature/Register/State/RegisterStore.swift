944310c68614347fb72a3b589db2f6db416923a8
ChoiAnYong <dksdyd78@naver.com>
2025-01-18 03:09:42 +0900

    feat: #80 SecondView 로직 추가

--- Spoony-iOS/Spoony-iOS/Source/Feature/Register/State/RegisterStore.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Register/State/RegisterStore.swift
@@ -6,12 +6,39 @@
 //
 
 import Foundation
+import SwiftUI
+import PhotosUI
+
+struct UploadImage: Identifiable {
+    let id = UUID()
+    let image: Image
+}
+
+enum UploadImageErrorState {
+    case initial
+    case noError
+    case error
+}
 
 final class RegisterStore: ObservableObject {
+    @Published var uploadImageErrorState: UploadImageErrorState = .initial
     @Published var step: RegisterStep = .start
     @Published var disableFirstButton: Bool = true
+    @Published var disableSecondButton: Bool = true
     @Published var text: String = ""
+    @Published var simpleReview: String = ""
+    @Published var detailReview: String = ""
     @Published var isSelected: Bool = false
+    @Published var simpleInputError: Bool = true {
+        didSet {
+            secondButtonInavlid()
+        }
+    }
+    @Published var detailInputError: Bool = true {
+        didSet {
+            secondButtonInavlid()
+        }
+    }
     @Published var categorys: [CategoryChip] = CategoryChip.sample()
     @Published var recommendMenu: [String] = [""] {
         didSet {
@@ -30,6 +57,29 @@ final class RegisterStore: ObservableObject {
         }
     }
     
+    @MainActor
+    @Published var pickerItem: PhotosPickerItem? {
+        didSet {
+            Task {
+                do {
+                    guard let data = try await pickerItem?.loadTransferable(type: Data.self),
+                          let uiImage = UIImage(data: data) else { return }
+                    uploadImages.append(.init(image: Image(uiImage: uiImage)))
+                } catch {
+                }
+            }
+        }
+    }
+    
+    @MainActor
+    @Published var uploadImages: [UploadImage] = [] {
+        didSet {
+            pickerItem = nil
+            uploadImageErrorState = .noError
+            secondButtonInavlid()
+        }
+    }
+
     func firstButtonInvalid() {
         let isRecommendMenuInvalid = recommendMenu.contains { $0.isEmpty }
         
@@ -38,5 +88,23 @@ final class RegisterStore: ObservableObject {
         let isSelectedPlaceInvalid = selectedPlace == nil
         
         disableFirstButton = isRecommendMenuInvalid || isSelectedCategoryInvalid || isSelectedPlaceInvalid
+    }
+    
+    func secondButtonInavlid() {
+        let uploadImageError = uploadImageErrorState == .error
+        disableSecondButton = simpleInputError || detailInputError || uploadImageError
+    }
+    
+    @MainActor
+    func deleteImage(_ image: UploadImage) {
+        guard let index = uploadImages.firstIndex(where: { $0.id == image.id }) else { return }
+        uploadImages.remove(at: index)
+        if uploadImages.isEmpty {
+            uploadImageErrorState = .error
+        }
+    }
+    
+    func isUploadImageError() -> Bool {
+        return uploadImageErrorState == .error
     }
 }
