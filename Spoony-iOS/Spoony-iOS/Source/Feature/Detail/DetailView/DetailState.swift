2ced85172a7694b37109c12d3200f41bf9d130df
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-01-23 15:16:45 +0900

    add: #130 - State 구조체 추가

--- Spoony-iOS/Spoony-iOS/Source/Feature/Detail/DetailView/DetailState.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Detail/DetailView/DetailState.swift
@@ -8,5 +8,28 @@
 import Foundation
 
 struct DetailState {
+    var postId: Int = 0
+    var userId: Int = 0
+    var userName: String = "이명진"
+    var photoUrlList: [String] = []
+    var title: String = ""
+    var date: String = ""
+    var menuList: [String] = []
+    var description: String = ""
+    var placeName: String = ""
+    var placeAddress: String = ""
+    var latitude: Double = 0.0
+    var longitude: Double = 0.0
+    var zzimCount: Int = 0
+    var isZzim: Bool = false
+    var isScoop: Bool = false
+    var categoryName: String = ""
+    var iconUrl: String = ""
+    var iconTextColor: String = ""
+    var iconBackgroundColor: String = ""
     
+    // 추가 상태
+    var isLoading: Bool = false
+    var errorMessage: String? = nil
+    var toast: Toast?
 }
