fb58b3e2d5fc888383c39c8d526892734a093444
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-02-07 19:41:36 +0900

    add: #179 - 실제 View 데이터로 사용 되는 DetailEntity 추가

---
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Detail/Domain/Entity/DetailEntity.swift
@@ -0,0 +1,54 @@
+//
+//  DetailEntity.swift
+//  Spoony-iOS
+//
+//  Created by 이명진 on 2/7/25.
+//
+
+import Foundation
+
+struct DetailEntity: Codable {
+    let postId: Int
+    let userId: Int
+    let photoUrlList: [String]
+    let title: String
+    let date: String
+    let menuList: [String]
+    let description: String
+    let placeName: String
+    let placeAddress: String
+    let latitude: Double
+    let longitude: Double
+    let zzimCount: Int
+    let isZzim: Bool
+    let isScoop: Bool
+    let categoryColorResponse: DetailCategoryColorResponse
+    let isMine: Bool
+    let spoonCount: Int
+    let userName: String
+    let userImageUrl: String
+    let regionName: String
+    
+    public init(reviewDetail: ReviewDetailModel, spoonCount: Int, userInfo: UserInfoModel) {
+        self.postId = reviewDetail.postId
+        self.userId = reviewDetail.userId
+        self.photoUrlList = reviewDetail.photoUrlList
+        self.title = reviewDetail.title
+        self.date = reviewDetail.date
+        self.menuList = reviewDetail.menuList
+        self.description = reviewDetail.description
+        self.placeName = reviewDetail.placeName
+        self.placeAddress = reviewDetail.placeAddress
+        self.latitude = reviewDetail.latitude
+        self.longitude = reviewDetail.longitude
+        self.zzimCount = reviewDetail.zzimCount
+        self.isZzim = reviewDetail.isZzim
+        self.isScoop = reviewDetail.isScoop
+        self.categoryColorResponse = reviewDetail.categoryColorResponse
+        self.isMine = reviewDetail.isMine
+        self.spoonCount = spoonCount
+        self.userName = userInfo.userName
+        self.userImageUrl = userInfo.userImageUrl
+        self.regionName = userInfo.regionName
+    }
+}
