e09a48b3ba7c9abbc050b1143a4ea4326fdcc607
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-02-14 18:26:28 +0900

    [Add] #179 - 문자열 자르는 Extension 추가

--- Spoony-iOS/Spoony-iOS/Resource/Extension/String+.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Extension/String+.swift
@@ -11,4 +11,9 @@ extension String {
     func splitZeroWidthSpace() -> String {
         return self.split(separator: "").joined(separator: "\u{200B}")
     }
+    
+    /// 날짜 문자열에서 yyyy-MM-dd 형식으로 변환하는 함수 (DataFormmatter 사용하는 것보다 효과적이라 적용함)
+    func toFormattedDateString() -> String {
+        return String(self.prefix(10))
+    }
 }
