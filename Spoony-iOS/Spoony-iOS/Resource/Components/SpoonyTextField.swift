f70ca490e5c90e5d1d0bfda8733e04b04a96264e
ChoiAnYong <dksdyd78@naver.com>
2025-02-14 16:00:43 +0900

    fix: 특정 이모지 입력시 버그 수정

--- Spoony-iOS/Spoony-iOS/Resource/Components/SpoonyTextField.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Components/SpoonyTextField.swift
@@ -84,7 +84,7 @@ extension SpoonyTextField {
             .foregroundStyle(.gray900)
             .onChange(of: text) { _, newValue in
                 if style != .icon {
-                    let removeText = newValue.removeCharacters()
+                    let removeText = newValue.removeEmogi()
                     
                     switch checkInputError(removeText) {
                     case .maximumInputError:
@@ -281,19 +281,11 @@ public enum TextFieldErrorState {
     }
 }
 
-
 extension String {
-    func removeCharacters() -> String {
-        return .init(
-            unicodeScalars.filter { scalar in
-                let isEmoji = scalar.properties.isEmoji || scalar.properties.isEmojiPresentation
-                let isControlCharacter = scalar.value == 8203 || scalar.value == 8204 || scalar.value == 8205 || scalar.value == 0x200B
-                let isSpecialCharacter = scalar == "*" || scalar == "#"
-                let isInvisibleSpace = scalar.value == 0x200B || scalar.value == 0x200C || scalar.value == 0x200D  // Invisible space characters
-                
-                return (!isEmoji && !isControlCharacter && !isInvisibleSpace) || scalar.properties.numericType == .decimal || isSpecialCharacter
-            }
-                .reduce(into: "") { $0 += String($1) }
-        )
+    func removeEmogi() -> String {
+        let regexString = "[^가-힣\\u3131-\\u314E\\u314F-\\u3163a-zA-Z0-9!@#$%^&*()_+\\-=\\[\\]{};':“”\"\\\\|,.<>/?£¥•~₩‘’\\s]+"
+        guard let regex = try? Regex(regexString) else { return self }
+        
+        return self.replacing(regex, with: "")
     }
 }
