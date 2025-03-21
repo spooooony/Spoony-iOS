4750de251e877bbafc918c953de0fca9006f210e
ChoiAnYong <dksdyd78@naver.com>
2025-03-21 19:55:33 +0900

    chore: #195 불필요한 코드 정리

--- Spoony-iOS/Spoony-iOS/Source/Feature/Register/Model/PlaceInfo.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Register/Model/PlaceInfo.swift
@@ -7,23 +7,11 @@
 
 import Foundation
 
-struct PlaceInfo {
+struct PlaceInfo: Equatable {
     let id = UUID()
     let placeName: String
     let placeAddress: String
     let placeRoadAddress: String
     let latitude: Double
     let longitude: Double
-}
-
-extension PlaceInfo {
-    static func sample() -> [PlaceInfo] {
-        return [
-//            .init(nameTitle: "신룽푸마라탕 고속터미널점", loaction: "서울 서초구 신반포로 194 지하 1층"),
-//            .init(nameTitle: "신룽푸마라탕 신라점", loaction: "서울 중구 신당동 416"),
-//            .init(nameTitle: "신룽푸마라탕 보라매점", loaction: "서울 동작구 보라매로5가길 16"),
-//            .init(nameTitle: "신룽푸마라탕 보라매점", loaction: "서울 동작구 보라매로5가길 16"),
-//            .init(nameTitle: "신룽푸마라탕 보라매점", loaction: "서울 동작구 보라매로5가길 16")
-        ]
-    }
 }
