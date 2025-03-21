78f916d62a68f9fceb49b1c37cc6975a3928589d
ChoiAnYong <dksdyd78@naver.com>
2025-03-21 19:59:55 +0900

    refactor: #195 SpoonyApp TCA 반영

--- Spoony-iOS/Spoony-iOS/SpoonyApp.swift
+++ Spoony-iOS/Spoony-iOS/SpoonyApp.swift
@@ -10,10 +10,15 @@ import SwiftUI
 import NMapsMap
 import KakaoSDKAuth
 import KakaoSDKCommon
+import ComposableArchitecture
 
 @main
 struct SpoonyApp: App {
     @StateObject private var navigationManager = NavigationManager()
+    
+    private let registerStore: StoreOf<RegisterFeature> = .init(initialState: .initialState) {
+        RegisterFeature()
+    }
     
     init() {
         NMFAuthManager.shared().clientId = Config.naverMapsClientId
@@ -22,7 +27,7 @@ struct SpoonyApp: App {
     
     var body: some Scene {
         WindowGroup {
-            SpoonyTabView()
+            SpoonyTabView(store: registerStore)
                 .environmentObject(navigationManager)
                 .popup(popup: $navigationManager.popup)
                 .onOpenURL(perform: { url in
