a640ced3d4956a8dc4988bbf038d2030adbc88a8
ChoiAnYong <dksdyd78@naver.com>
2025-03-21 19:53:49 +0900

    feat: #195 RegisterStep 구현

---
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Register/Enums/RegisterStep.swift
@@ -0,0 +1,14 @@
+//
+//  RegisterStep.swift
+//  Spoony-iOS
+//
+//  Created by 최안용 on 3/21/25.
+//
+
+import Foundation
+
+enum RegisterStep: Int {
+    case start = 1
+    case middle = 2
+    case end = 3
+}
