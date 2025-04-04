5ff49fc40d578a593dc637a78c288c3dd82898a5
JURI <80569323+juri123123@users.noreply.github.com>
2025-04-05 01:28:24 +0900

    style: #209 닉네임 텍스트 필드 UI 구현

--- Spoony-iOS/Spoony-iOS/Resource/Extension/String+.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Extension/String+.swift
@@ -19,6 +19,14 @@ extension String {
         return self.replacing(regex, with: "")
     }
     
+    func removeSpecialCharacter() -> String {
+        let regexString = "[^a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣\\s]"
+        
+        guard let regex = try? Regex(regexString) else { return self }
+        
+        return self.replacing(regex, with: "")
+    }
+    
     /// 날짜 문자열에서 yyyy-MM-dd 형식으로 변환하는 함수 (DataFormmatter 사용하는 것보다 효과적이라 적용함)
     func toFormattedDateString() -> String {
         return String(self.prefix(10))
