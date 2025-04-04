30d1e3f96d0ed7f3b0f1962d81aed36e5e9eaca4
hooni <jhlee909090@gmail.com>
2025-04-04 13:16:11 +0900

    style: #211- 기본 컴포넌트 완성

--- Spoony-iOS/Spoony-iOS/Resource/Components/SpoonyDraw/SpoonDrawPopupView.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Components/SpoonyDraw/SpoonDrawPopupView.swift
@@ -7,17 +7,11 @@
 
 import SwiftUI
 
-extension View {
-    func height(_ height: CGFloat) -> some View {
-        self.frame(height: height)
-    }
-}
-
 struct SpoonDrawPopupView: View {
     @Binding var isPresented: Bool
+    
     var body: some View {
         ZStack {
-            // 배경 딤 처리
             if isPresented {
                 Color.black.opacity(0.4)
                     .ignoresSafeArea()
@@ -26,7 +20,6 @@ struct SpoonDrawPopupView: View {
                     }
             }
             
-            // 팝업 내용
             VStack(spacing: 0) {
                 // 닫기 버튼 영역
                 HStack {
@@ -34,54 +27,49 @@ struct SpoonDrawPopupView: View {
                     Button(action: {
                         isPresented = false
                     }) {
-                        Image(systemName: "xmark")
-                            .foregroundColor(.gray)
-                            .padding(.top, 16)
-                            .padding(.trailing, 16)
+                        Image(.icCloseGray400)
+                            .padding(.top, 15)
+                            .padding(.trailing, 20)
                     }
                 }
                 
                 // 제목
-                Text("오늘의 스폰 뽑기")
-                    .font(.system(size: 18, weight: .bold))
-                    .padding(.top, 8)
+                Text("오늘의 스푼 뽑기")
+                //TODO: 폰트 나중에 주리꺼로~
+                    .font(.title1b)
+                    .padding(.top, 0)
                 
                 // 설명 텍스트
-                Text("스폰 뽑기 버튼을 누르면\n오늘의 스폰을 획득할 수 있어요.")
-                    .font(.system(size: 14))
+                Text("'스푼 뽑기' 버튼을 누르면\n오늘의 스폰을 획득할 수 있어요.")
+                    .font(.body2sb)
                     .multilineTextAlignment(.center)
-                    .foregroundColor(.gray)
-                    .padding(.top, 8)
+                    .foregroundStyle(.gray600)
+                    .padding(.top, 16)
                 
-                // 체크보드 이미지 영역 (그레이 체크무늬 영역)
+                //TODO: 로띠
                 Rectangle()
-                   
                     .aspectRatio(1, contentMode: .fit)
                     .padding(.horizontal, 36)
                     .padding(.vertical, 20)
-                
-                // 버튼
+            
                 Button(action: {
-                    // 스폰 뽑기 버튼 액션
+               //TODO: 스폰 뽑기 버튼 액션 추가
                 }) {
-                    Text("스폰 뽑기")
-                        .font(.system(size: 16, weight: .bold))
+                    Text("스푼 뽑기")
+                        .font(.body2b)
                         .foregroundColor(.white)
                         .frame(maxWidth: .infinity)
                         .padding(.vertical, 16)
-                        .background(Color.orange)
-                        .cornerRadius(8)
+                        .background(.main400)
+                        .cornerRadius(10)
                 }
-                .padding(.horizontal, 36)
-                .padding(.bottom, 24)
+                .padding(.horizontal, 20)
+                .padding(.bottom, 20)
             }
             .background(Color.white)
-            .cornerRadius(12)
-            .height(303)
+            .cornerRadius(16)
             .padding(.horizontal, 36)
-            .opacity(isPresented ? 1 : 0)
-            .scaleEffect(isPresented ? 1 : 0.5)
-            .animation(.spring(), value: isPresented)
+
         }
     }
 }
