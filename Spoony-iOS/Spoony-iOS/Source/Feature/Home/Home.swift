fb29699685d7fbd6e1383e03a22243065e45d056
hooni <jhlee909090@gmail.com>
2025-04-10 13:11:28 +0900

    style:218 전체 컴포넌트 하드코딩으로 넣기

--- Spoony-iOS/Spoony-iOS/Source/Feature/Home/Home.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Home/Home.swift
@@ -59,8 +59,10 @@ struct Home: View {
                 )
                 .frame(height: 56.adjusted)
                 
+                // 카테고리 버튼 영역 (일자로 배치)
                 HStack(spacing: 8) {
                     if categories.isEmpty {
+                        // 로딩 중일 때 플레이스홀더 표시
                         ForEach(0..<4, id: \.self) { _ in
                             CategoryChipsView(category: CategoryChip.placeholder)
                                 .redacted(reason: .placeholder)
@@ -68,6 +70,21 @@ struct Home: View {
                     } else {
                         ScrollView(.horizontal, showsIndicators: false) {
                             HStack(spacing: 8) {
+                                // 일단 하드코딩 박기ㅋㅋ
+                                CategoryChipsView(
+                                    category: CategoryChip(
+                                        image: "https://spoony-storage.s3.ap-northeast-2.amazonaws.com/category/icons/all_color.png",
+                                        selectedImage: "https://spoony-storage.s3.ap-northeast-2.amazonaws.com/category/icons/all_white.png",
+                                        title: "전체",
+                                        id: 0
+                                    ),
+                                    isSelected: selectedCategories.isEmpty || selectedCategories.contains { $0.id == 0 }
+                                )
+                                .onTapGesture {
+                                    // 전체를 선택하면 다른 선택을 모두 해제
+                                    selectedCategories = []
+                                }
+                                
                                 ForEach(categories, id: \.id) { category in
                                     CategoryChipsView(
                                         category: category,
@@ -114,13 +131,15 @@ struct Home: View {
                 
                 let categoryResponse = try await registerService.getRegisterCategories()
                 categories = try await categoryResponse.toModel()
+                
+               
             } catch {
                 print("Failed to fetch data:", error)
             }
         }
     }
     
-    // 카테고리 선택 처리
+    // 카테고리 선택 처리 메서드
     private func handleCategorySelection(_ category: CategoryChip) {
         if selectedCategories.contains(where: { $0.id == category.id }) {
             selectedCategories.removeAll { $0.id == category.id }
@@ -128,7 +147,6 @@ struct Home: View {
             selectedCategories = [category]
         }
         
-        //TODO: 필터링 로직 구현
         if !selectedCategories.isEmpty {
             let categoryId = selectedCategories[0].id
             print("Selected category: \(categoryId)")
