912eb22e6043fff9d2d031b3925a57bc6adba7b5
hooni <jhlee909090@gmail.com>
2025-04-13 21:27:16 +0900

    feat: #220 리뷰 뷰 피쳐 연결

---
+++ Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/Review/Review.swift
@@ -0,0 +1,38 @@
+//
+//  Review.swift
+//  Spoony-iOS
+//
+//  Created by 이지훈 on 4/13/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+import TCACoordinators
+
+struct ReviewsView: View {
+    @Bindable private var store: StoreOf<ReviewsFeature>
+    
+    init(store: StoreOf<ReviewsFeature>) {
+        self.store = store
+    }
+    
+    var body: some View {
+        VStack {
+            CustomNavigationBar(
+                style: .detail,
+                searchText: .constant(""),
+                onBackTapped: {
+                    store.send(.routeToPreviousScreen)
+                }
+            )
+            
+            Spacer()
+            
+            Text("Reviews View")
+                .font(.largeTitle)
+            
+            Spacer()
+        }
+    }
+}
