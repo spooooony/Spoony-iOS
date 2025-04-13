582914c16ef5a0a4c38ea1428cf3bf13887a6cd0
hooni <jhlee909090@gmail.com>
2025-04-13 21:26:39 +0900

    feat: #220 각 페이지 피쳐 연결

--- Spoony-iOS/Spoony-iOS/Source/Feature/Coordinator/TestScenes/MyPageCoordinator/MyPageCoordinator.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Coordinator/TestScenes/MyPageCoordinator/MyPageCoordinator.swift
@@ -9,7 +9,6 @@ import Foundation
 import ComposableArchitecture
 import TCACoordinators
 
-
 @Reducer
 struct MyPageCoordinator {
     @ObservableState
@@ -30,7 +29,33 @@ struct MyPageCoordinator {
                 state.routes.push(.reviews(.initialState))
                 return .none
                 
-            case .router(.routeAction(id: _, action: .reviews(.routeToPreviousScreen))):
+            case .router(.routeAction(id: _, action: .profile(.routeToFollowingScreen))):
+                state.routes.push(.following(.initialState))
+                return .none
+                
+            case .router(.routeAction(id: _, action: .profile(.routeToFollowerScreen))):
+                state.routes.push(.follower(.initialState))
+                return .none
+                
+            case .router(.routeAction(id: _, action: .profile(.routeToEditProfileScreen))):
+                state.routes.push(.editProfile(.initialState))
+                return .none
+                
+            case .router(.routeAction(id: _, action: .profile(.routeToSettingsScreen))):
+                state.routes.push(.settings(.initialState))
+                return .none
+                
+            case .router(.routeAction(id: _, action: .profile(.routeToAttendanceScreen))):
+                state.routes.push(.attendance(.initialState))
+                return .none
+                
+            // 이전 화면으로 돌아가기
+            case .router(.routeAction(id: _, action: .reviews(.routeToPreviousScreen))),
+                 .router(.routeAction(id: _, action: .following(.routeToPreviousScreen))),
+                 .router(.routeAction(id: _, action: .follower(.routeToPreviousScreen))),
+                 .router(.routeAction(id: _, action: .editProfile(.routeToPreviousScreen))),
+                 .router(.routeAction(id: _, action: .settings(.routeToPreviousScreen))),
+                 .router(.routeAction(id: _, action: .attendance(.routeToPreviousScreen))):
                 state.routes.goBack()
                 return .none
                 
