10b77562a3ed4596428c8b0198507d2eca2308e3
hooni <jhlee909090@gmail.com>
2025-04-13 21:29:12 +0900

    feat: #220 출석하기 뷰 피쳐 연결

---
+++ Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/Edit/EditProfileFeature.swift
@@ -0,0 +1,31 @@
+//
+//  EditProfileFeature.swift
+//  Spoony-iOS
+//
+//  Created by 이지훈 on 4/13/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+import TCACoordinators
+@Reducer
+struct EditProfileFeature {
+    @ObservableState
+    struct State: Equatable {
+        static let initialState = State()
+    }
+    
+    enum Action {
+        case routeToPreviousScreen
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .routeToPreviousScreen:
+                return .none
+            }
+        }
+    }
+}
