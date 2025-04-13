e835385bb315380fb411e33b0fe0afc89da08727
hooni <jhlee909090@gmail.com>
2025-04-13 21:27:02 +0900

    feat: #220 출석뷰 피쳐 연결

---
+++ Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/Setting/SettingsView.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsView.swift
+//  Spoony-iOS
+//
+//  Created by 이지훈 on 4/13/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+import TCACoordinators
+
+struct SettingsView: View {
+    @Bindable private var store: StoreOf<SettingsFeature>
+    
+    init(store: StoreOf<SettingsFeature>) {
+        self.store = store
+    }
+    
+    var body: some View {
+        VStack {
+            CustomNavigationBar(
+                style: .detail,
+                searchText: .constant(""),
+                onBackTapped: {
+                    store.send(.routeToPreviousScreen)
+                }
+            )
+            
+            Spacer()
+            
+            Text("Settings View")
+                .font(.largeTitle)
+            
+            Spacer()
+        }
+    }
+}
