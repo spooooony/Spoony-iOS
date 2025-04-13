e4332974aae51e1c0cf36929765e9aa373d6bbeb
JURI <80569323+juri123123@users.noreply.github.com>
2025-04-13 17:47:06 +0900

    style: #209 온보딩 완료 UI 구현

---
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Onboarding/View/OnboardingFinishView.swift
@@ -0,0 +1,48 @@
+//
+//  OnboardingFinishView.swift
+//  Spoony-iOS
+//
+//  Created by 최주리 on 4/13/25.
+//
+
+import SwiftUI
+
+struct OnboardingFinishView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            HStack(spacing: 6) {
+                Text("반가워요,")
+                Text("\("제이예요")")
+                    .foregroundStyle(.main400)
+                Text("님!")
+            }
+            .customFont(.title2)
+            .padding(.top, 62)
+            Text("이제 스푸니를 시작해 볼까요?")
+                .customFont(.title2)
+                .padding(.top, 4)
+            
+            // TODO: Lottie
+            Rectangle()
+                .frame(width: 335.adjusted, height: 406.adjustedH)
+                .foregroundStyle(.gray200)
+                .padding(.top, 37)
+            Spacer()
+            
+            VStack(spacing: 16) {
+                Text("내 프로필 정보는 마이페이지에서 변경할 수 있어요")
+                    .customFont(.caption1m)
+                    .foregroundStyle(.gray400)
+                
+                SpoonyButton(style: .primary, size: .xlarge, title: "스푸니 시작하기 ", disabled: .constant(false)) {
+                    // TODO: 네비게이션
+                }
+            }
+            .padding(.bottom, 20)
+        }
+    }
+}
+
+#Preview {
+    OnboardingFinishView()
+}
