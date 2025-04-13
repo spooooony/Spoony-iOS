d42866bbbc766eeda0d89417e8a7178f52d693a8
hooni <jhlee909090@gmail.com>
2025-04-13 21:29:44 +0900

    feat: #220 마이페이지 각 리듀서 연결

--- Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/MyPageView.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/MyPageView.swift
@@ -14,6 +14,11 @@ import TCACoordinators
 enum MyPageScreen {
     case profile(ProfileFeature)
     case reviews(ReviewsFeature)
+    case following(FollowingFeature)
+    case follower(FollowerFeature)
+    case editProfile(EditProfileFeature)
+    case settings(SettingsFeature)
+    case attendance(AttendanceFeature)
 }
 
 struct MyPageView: View {
@@ -24,13 +29,23 @@ struct MyPageView: View {
     }
     
     var body: some View {
-        TCARouter(store.scope(state: \.routes, action: \.router)) { screen in
-            switch screen.case {
-            case let .profile(store):
-                ProfileView(store: store)
-            case let .reviews(store):
-                ReviewsView(store: store)
+            TCARouter(store.scope(state: \.routes, action: \.router)) { screen in
+                switch screen.case {
+                case let .profile(store):
+                    ProfileView(store: store)
+                case let .reviews(store):
+                    ReviewsView(store: store)
+                case let .following(store):
+                    FollowingView(store: store)
+                case let .follower(store):
+                    FollowerView(store: store)
+                case let .editProfile(store):
+                    EditProfileView(store: store)
+                case let .settings(store):
+                    SettingsView(store: store)
+                case let .attendance(store):
+                    AttendanceView(store: store)
+                }
             }
         }
     }
-}
