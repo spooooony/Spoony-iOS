d09cf6bea3dded65710ea7dfad5471db2b2dabe0
JURI <80569323+juri123123@users.noreply.github.com>
2025-04-14 21:58:21 +0900

    style: #226 탐색 cell UI

--- Spoony-iOS/Spoony-iOS/Source/Feature/Explore/Model/FeedEntity.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Explore/Model/FeedEntity.swift
@@ -22,4 +22,20 @@ struct FeedEntity: Identifiable, Hashable {
     let zzimCount: Int
 }
 
+struct FeedEntity_2: Identifiable, Hashable {
+    static func == (lhs: FeedEntity_2, rhs: FeedEntity_2) -> Bool {
+        lhs.id == rhs.id
+    }
+    
+    let id: UUID
+    let postId: Int
+    let userName: String
+    let userRegion: String
+    let description: String
+    let categorColorResponse: CategoryColorResponse
+    let zzimCount: Int
+    let photoURLList: [String]
+    let createAt: String
+}
+
 
