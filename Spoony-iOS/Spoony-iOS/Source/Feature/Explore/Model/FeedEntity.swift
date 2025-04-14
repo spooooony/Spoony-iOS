66ec239804f43fe43c69b07c7c96fbdc20222008
JURI <80569323+juri123123@users.noreply.github.com>
2025-04-14 23:40:20 +0900

    fix: feed 모델 변경

--- Spoony-iOS/Spoony-iOS/Source/Feature/Explore/Model/FeedEntity.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Explore/Model/FeedEntity.swift
@@ -8,22 +8,7 @@
 import Foundation
 
 struct FeedEntity: Identifiable, Hashable {
-    
     static func == (lhs: FeedEntity, rhs: FeedEntity) -> Bool {
-        lhs.id == rhs.id
-    }
-    
-    let id: UUID
-    let postId: Int
-    let userName: String
-    let userRegion: String
-    let title: String
-    let categorColorResponse: CategoryColorResponse
-    let zzimCount: Int
-}
-
-struct FeedEntity_2: Identifiable, Hashable {
-    static func == (lhs: FeedEntity_2, rhs: FeedEntity_2) -> Bool {
         lhs.id == rhs.id
     }
     
