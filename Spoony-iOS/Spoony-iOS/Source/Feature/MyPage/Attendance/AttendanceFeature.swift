745a72d4b4b03f0e143a84ae00387a9e0b6c54af
hooni <jhlee909090@gmail.com>
2025-04-17 16:04:16 +0900

    feat: #241 출석뷰 연결

---
+++ Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/Attendance/AttendanceFeature.swift
@@ -0,0 +1,31 @@
+//
+//  AttendanceFeature.swift
+//  Spoony-iOS
+//
+//  Created by 이지훈 on 4/17/25.
+//
+
+import Foundation
+
+import ComposableArchitecture
+
+@Reducer
+struct AttendanceFeature {
+    @ObservableState
+    struct State: Equatable {
+        static let initialState = State()
+    }
+    
+    enum Action {
+        case routeToPreviousScreen
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            default:
+                return .none
+            }
+        }
+    }
+}
--- Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/MyPageView.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/MyPageView.swift
@@ -29,23 +29,26 @@ struct MyPageView: View {
     }
     
     var body: some View {
-            TCARouter(store.scope(state: \.routes, action: \.router)) { screen in
-                switch screen.case {
-                case let .profile(store):
-                    ProfileView(store: store)
-                case let .reviews(store):
-                    ReviewsView(store: store)
-                case let .following(store):
-                    FollowingView(store: store)
-                case let .follower(store):
-                    FollowerView(store: store)
-                case let .editProfile(store):
-                    EditProfileView(store: store)
-                case let .settings(store):
-                    SettingsView(store: store)
-                case let .attendance(store):
-                    AttendanceView(store: store)
-                }
+        TCARouter(store.scope(state: \.routes, action: \.router)) { screen in
+            switch screen.case {
+            case let .profile(store):
+                ProfileView(store: store)
+            case let .reviews(store):
+                ReviewsView(store: store)
+            case let .following(store):
+                FollowingView(store: store)
+            case let .follower(store):
+                FollowerView(store: store)
+            case let .editProfile(store):
+                EditProfileView(store: store)
+            case let .settings(store):
+                SettingsView(store: store)
+            case let .attendance(store):
+                MealTrackerView()
+                    .onDisappear {
+                        store.send(.routeToPreviousScreen)
+                    }
             }
         }
     }
+}
