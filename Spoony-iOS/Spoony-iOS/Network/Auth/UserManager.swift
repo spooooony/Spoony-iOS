fb3b44d5a9db58b74d24476054ba920195ebd9fd
JURI <80569323+juri123123@users.noreply.github.com>
2025-04-19 08:34:31 +0900

    feat: #228 userManager 최근 검색어 함수 추가

--- Spoony-iOS/Spoony-iOS/Network/Auth/UserManager.swift
+++ Spoony-iOS/Spoony-iOS/Network/Auth/UserManager.swift
@@ -12,7 +12,66 @@ final class UserManager {
     
     @UserDefaultWrapper(key: "RecentSearches") public var recentSearches: [String]?
     
+    @UserDefaultWrapper(key: "exploreUserRecentSearches") public var exploreUserRecentSearches: [String]?
+    @UserDefaultWrapper(key: "exploreReviewRecentSearches") public var exploreReviewRecentSearches: [String]?
+    
     static let shared = UserManager()
     
     private init() { }
+    
+    func setSearches(_ key: String, _ text: String) {
+        var list: [String]?
+        
+        switch key {
+        case "exploreUserRecentSearches":
+            list = exploreUserRecentSearches ?? []
+        case "exploreReviewRecentSearches":
+            list = exploreReviewRecentSearches ?? []
+        default:
+            return
+        }
+        
+        if let index = list?.firstIndex(of: text) {
+            list?.remove(at: index)
+        } else if list?.count == 6 {
+            list?.removeLast()
+        }
+        
+        list?.insert(text, at: 0)
+        
+        switch key {
+        case "exploreUserRecentSearches":
+            exploreUserRecentSearches = list
+        case "exploreReviewRecentSearches":
+            exploreReviewRecentSearches = list
+        default:
+            return
+        }
+    }
+    
+    func deleteRecent(_ key: String, _ text: String) {
+        switch key {
+        case "exploreUserRecentSearches":
+            guard let index = exploreUserRecentSearches?.firstIndex(of: text)
+            else { return }
+            
+            exploreUserRecentSearches?.remove(at: index)
+        case "exploreReviewRecentSearches":
+            guard let index = exploreReviewRecentSearches?.firstIndex(of: text)
+            else { return }
+            
+            exploreReviewRecentSearches?.remove(at: index)
+        default:
+            return
+        }
+    }
+}
+
+// TODO: 리터럴 -> enum key값 관리하기
+// 여러 파일 건들여야 해서 추후 수정 하겠습니다 !
+enum UserDefaultsKey: String {
+    case userId
+    case recentSearches
+    case exploreUserRecentSearches
+    case exploreReviewRecentSearches
 }
