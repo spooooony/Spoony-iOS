606171f4fdbda637d910b7ef195767432b0bbde5
ChoiAnYong <dksdyd78@naver.com>
2025-04-26 15:20:32 +0900

    feat: #244 - 수정 모드 로직 추가

--- Spoony-iOS/Spoony-iOS/Source/Feature/Register/State/RegisterFeature.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Register/State/RegisterFeature.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import Foundation
 
 @Reducer
 struct RegisterFeature {
@@ -14,6 +15,11 @@ struct RegisterFeature {
         static let initialState = State(
             infoStepState: .initialState,
             reviewStepState: .initialState
+        )
+        
+        static let editState = State(
+            infoStepState: .editState,
+            reviewStepState: .editState
         )
         
         var currentStep: RegisterStep = .start
@@ -25,6 +31,7 @@ struct RegisterFeature {
     
     enum Action: BindableAction, Equatable {
         case binding(BindingAction<State>)
+        case onAppear
         case updateIsLoading(Bool)
         case updateStep(RegisterStep)
         case resetState
@@ -38,6 +45,8 @@ struct RegisterFeature {
         // MARK: - TabCoordinator Action
         case presentPopup
         case presentToast(message: String)
+        
+        case routeToPreviousScreen
     }
         
     @Dependency(\.registerService) var network: RegisterServiceType
@@ -55,8 +64,36 @@ struct RegisterFeature {
         
         Reduce { state, action in
             switch action {
+            case .onAppear:
+                if state.infoStepState.isEditMode {
+                    // TODO: - 게시물 정보 API 호출
+                    state.infoStepState.selectedCategory.append(
+                        .init(
+                            image: "https://spoony-storage.s3.ap-northeast-2.amazonaws.com/category/icons/korean_black.png",
+                            selectedImage: "https://spoony-storage.s3.ap-northeast-2.amazonaws.com/category/icons/korean_white.png",
+                            title: "한식",
+                            id: 3
+                        )
+                    )
+                    
+                    state.infoStepState.recommendTexts = [RecommendText(text: "아메리카노")]
+                    state.infoStepState.selectedPlace = .init(
+                        placeName: "스타벅스 한국프레스센터점",
+                        placeAddress: "서울특별시 중구 태평로1가 25",
+                        placeRoadAddress: "모름",
+                        latitude: 37.5672475,
+                        longitude: 126.9780493
+                    )
+                    state.infoStepState.satisfaction = 90.0
+                    state.reviewStepState.detailText = "한 달에 5번은 먹는 마라탕 집이에요. 마라 향 센 걸 잘 못 먹는 사람들한테 추천해용 친구, 가족 모두한테 소개해 줬는데 다들 좋아해요!"
+                    state.reviewStepState.uploadImages.append(UploadImage(image: ImageType(.imgMockGodeung), imageData: Data()))
+                    
+                    state.reviewStepState.selectableCount = 4
+                }
+                return .none
             case .updateIsLoading(let isLoading):
                 state.isLoading = isLoading
+                
                 return .none
             case .updateStep(let step):
                 state.currentStep = step
@@ -74,9 +111,10 @@ struct RegisterFeature {
                     return .none
                 }
                 
+                // TODO: - 수정 등록 분기
                 let request = RegisterPostRequest(
                     userId: Config.userId,
-                    title: state.reviewStepState.simpleText,
+                    title: "",
                     description: state.reviewStepState.detailText,
                     placeName: selectedPlace.placeName,
                     placeAddress: selectedPlace.placeAddress,
@@ -122,7 +160,13 @@ struct RegisterFeature {
             case .registrationSuccessful:
                 state.currentStep = .end
                 state.isRegistrationSuccess = true
-                return .send(.presentPopup)
+                
+                if state.reviewStepState.isEditMode {
+                    // TODO: - 디테일 화면으로 이동
+                    return .send(.routeToPreviousScreen)
+                } else {
+                    return .send(.presentPopup)
+                }
             case .onDisappear:
                 if state.isRegistrationSuccess {
                     return .send(.resetState)
