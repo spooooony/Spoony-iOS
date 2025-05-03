9fe0ab13f1e6e32e8ae36aec0611abdcbd4da64c
hooni <jhlee909090@gmail.com>
2025-05-03 16:28:32 +0900

    feat: #268 계정관리에 대한 화면이동 경우의수

--- Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/Setting/AccountManagement/AccountManagementFeature.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/Setting/AccountManagement/AccountManagementFeature.swift
@@ -10,19 +10,44 @@ import ComposableArchitecture
 
 @Reducer
 struct AccountManagementFeature {
+    enum LoginType: Equatable {
+        case apple
+        case kakao
+    }
+    
     @ObservableState
     struct State: Equatable {
-        static let initialState = State()
+        static let initialState = State(currentLoginType: .kakao)
+        
+        var currentLoginType: LoginType
     }
     
     enum Action {
         case routeToPreviousScreen
+        case selectLoginType(LoginType)
+        case logoutButtonTapped
+        case withdrawButtonTapped
+        case routeToLogoutScreen
+        case routeToWithdrawScreen
     }
     
     var body: some ReducerOf<Self> {
-        Reduce { _, action in
+        Reduce { state, action in
             switch action {
             case .routeToPreviousScreen:
+                return .none
+                
+            case let .selectLoginType(type):
+                state.currentLoginType = type
+                return .none
+                
+            case .logoutButtonTapped:
+                return .send(.routeToLogoutScreen)
+                
+            case .withdrawButtonTapped:
+                return .send(.routeToWithdrawScreen)
+                
+            case .routeToLogoutScreen, .routeToWithdrawScreen:
                 return .none
             }
         }
