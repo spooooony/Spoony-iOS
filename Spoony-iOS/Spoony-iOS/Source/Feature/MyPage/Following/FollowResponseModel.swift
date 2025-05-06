174992b39e3b67d462437532ae3c259580d439c7
LeeMyeongJin <dlaudwls1203@gmail.com>
2025-05-06 20:18:39 +0900

    add: #252 - FollowResponseModel 추가

--- Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/Following/FollowResponseModel.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/MyPage/Following/FollowResponseModel.swift
@@ -7,26 +7,29 @@
 
 import Foundation
 
-struct FollowResponseModel: Codable {
-    let follows: [FollowModel]
+struct FollowListDTO: Codable, Equatable {
+    let count: Int
+    let users: [FollowUserDTO]
 }
 
-struct FollowModel: Codable {
+struct FollowUserDTO: Codable, Equatable {
     let userId: Int
     let username: String
-    let location: String
+    let regionName: String
     let isFollowing: Bool
+    let profileImageUrl: String
 }
 
-extension FollowModel {
-    static func dummyData() -> [FollowModel] {
-        return [
-            FollowModel(userId: 1, username: "배가희", location: "마포구", isFollowing: true),
-            FollowModel(userId: 2, username: "최주리", location: "은평구", isFollowing: true),
-            FollowModel(userId: 3, username: "이지훈", location: "강남구", isFollowing: false),
-            FollowModel(userId: 4, username: "최안용", location: "어쩌라구", isFollowing: false),
-            FollowModel(userId: 5, username: "양수정", location: "서대문구", isFollowing: true),
-            FollowModel(userId: 6, username: "김세은", location: "용산구", isFollowing: true)
+extension FollowListDTO {
+    static let sample = FollowListDTO(
+        count: 6,
+        users: [
+            FollowUserDTO(userId: 1, username: "배가희", regionName: "마포구", isFollowing: true, profileImageUrl: "https://example.com/profile1.png"),
+            FollowUserDTO(userId: 2, username: "최주리", regionName: "은평구", isFollowing: true, profileImageUrl: "https://example.com/profile2.png"),
+            FollowUserDTO(userId: 3, username: "이지훈", regionName: "강남구", isFollowing: false, profileImageUrl: "https://example.com/profile3.png"),
+            FollowUserDTO(userId: 4, username: "최안용", regionName: "어쩌라구", isFollowing: false, profileImageUrl: "https://example.com/profile4.png"),
+            FollowUserDTO(userId: 5, username: "양수정", regionName: "서대문구", isFollowing: true, profileImageUrl: "https://example.com/profile5.png"),
+            FollowUserDTO(userId: 6, username: "김세은", regionName: "용산구", isFollowing: true, profileImageUrl: "https://example.com/profile6.png")
         ]
-    }
+    )
 }
