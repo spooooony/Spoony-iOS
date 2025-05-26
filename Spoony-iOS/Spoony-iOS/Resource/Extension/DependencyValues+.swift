da5d71cbe8cb03092ecf9004a1be390ed09a730f
hooni <jhlee909090@gmail.com>
2025-05-26 17:34:09 +0900

    hofix: 컨플릭트 해결이후 괄호 안맞는 문제 해결

--- Spoony-iOS/Spoony-iOS/Resource/Extension/DependencyValues+.swift
+++ Spoony-iOS/Spoony-iOS/Resource/Extension/DependencyValues+.swift
@@ -53,14 +53,13 @@ extension DependencyValues {
         set { self[ReportServiceKey.self] = newValue }
     }
     
-
     var blockService: BlockServiceProtocol {
         get { self[BlockServiceKey.self] }
         set { self[BlockServiceKey.self] = newValue }
-
+    }
+    
     var spoonDrawService: SpoonDrawServiceProtocol {
         get { self[SpoonDrawServiceKey.self] }
         set { self[SpoonDrawServiceKey.self] = newValue }
-
     }
 }
