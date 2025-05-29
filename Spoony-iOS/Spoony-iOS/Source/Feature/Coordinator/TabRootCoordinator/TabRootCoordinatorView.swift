a4c73dbd9945e9a80ccaa86739b3e83a3c8c30ff
ChoiAnYong <dksdyd78@naver.com>
2025-05-29 15:38:37 +0900

    feat: #302 - TabRootCoordinatorView 구현

---
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Coordinator/TabRootCoordinator/TabRootCoordinatorView.swift
@@ -0,0 +1,61 @@
+//
+//  TabRootCoordinatorView.swift
+//  Spoony-iOS
+//
+//  Created by 최안용 on 5/27/25.
+//
+
+import SwiftUI
+
+import ComposableArchitecture
+import TCACoordinators
+
+struct TabRootCoordinatorView: View {
+    private let store: StoreOf<TabRootCoordinator>
+    
+    init(store: StoreOf<TabRootCoordinator>) {
+        self.store = store
+    }
+    
+    var body: some View {
+        TCARouter(store.scope(state: \.routes, action: \.router)) { screen in
+            switch screen.case {
+            case let .tab(store):
+                TabCoordinatorView(store: store)
+                
+            case let .registerAndEdit(store: store):
+                Register(store: store)
+                    .toolbar(.hidden, for: .navigationBar)
+                
+            case let .settings(store):
+                SettingsView(store: store)
+                
+            case let .editProfile(store):
+                EditProfileView(store: store)
+                
+            case let .attendance(store):
+                AttendanceView(store: store)
+                    .navigationBarBackButtonHidden()
+                    .toolbar(.hidden, for: .tabBar)
+                
+            case let .accountManagement(store):
+                AccountManagementView(store: store)
+                    .navigationBarBackButtonHidden()
+                
+            case let .blockedUsers(store):
+                BlockedUsersView(store: store)
+                    .navigationBarBackButtonHidden()
+                
+            case let .withdraw(store):
+                WithdrawView(store: store)
+                    .navigationBarBackButtonHidden()
+                
+            case let .detail(store):
+                PostView(postId: store.postId, store: store)
+                
+            case let .report(store):
+                Report(postId: store.postId, userId: nil, store: store)
+            }
+        }
+    }
+}
