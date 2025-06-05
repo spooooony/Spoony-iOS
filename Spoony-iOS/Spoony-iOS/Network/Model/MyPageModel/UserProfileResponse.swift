ff68d8c841b9a5e33b90f87527158eeb8806489a
hooni <jhlee909090@gmail.com>
2025-06-05 10:58:33 +0900

    fix: #354 안용 프로필 피쳐 DTO 수정

--- Spoony-iOS/Spoony-iOS/Network/Model/MyPageModel/UserProfileResponse.swift
+++ Spoony-iOS/Spoony-iOS/Network/Model/MyPageModel/UserProfileResponse.swift
@@ -9,26 +9,39 @@ import Foundation
 
 struct UserProfileResponse: Codable {
     let userName: String
-    let regionName: String
+    let regionName: String?
     let introduction: String
-    let birth: String
+    let birth: String?
     let imageLevel: Int
 }
 
 extension UserProfileResponse {
     func toModel() -> ProfileInfo {
-        let birthDateComponents: [String] = birth.split(separator: "-").map { String($0) }
+        let birthDateComponents: [String]
+        if let birth = birth, !birth.isEmpty {
+            birthDateComponents = birth.split(separator: "-").map { String($0) }
+        } else {
+            birthDateComponents = ["", "", ""]
+        }
         
-        let regionNameComponents: [String] = regionName.split(separator: " ").map { String($0) }
+        let location: LocationType
+        let finalRegionName: String
         
-        let location = LocationType.allCases.first(where: { $0.rawValue == regionNameComponents[0] })
+        if let regionName = regionName, !regionName.isEmpty {
+            let regionNameComponents: [String] = regionName.split(separator: " ").map { String($0) }
+            location = LocationType.allCases.first(where: { $0.rawValue == regionNameComponents[0] }) ?? .seoul
+            finalRegionName = regionName
+        } else {
+            location = .seoul
+            finalRegionName = ""
+        }
         
         return .init(
             nickname: userName,
             introduction: introduction,
             birthDate: birthDateComponents,
-            regionName: regionName,
-            selectedLocation: location ?? .seoul,
+            regionName: finalRegionName,
+            selectedLocation: location,
             selectedSubLocation: nil,
             imageLevel: imageLevel
         )
