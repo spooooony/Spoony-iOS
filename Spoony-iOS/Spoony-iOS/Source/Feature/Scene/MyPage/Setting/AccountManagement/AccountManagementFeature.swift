5f770df5f7fe79b2323debd60f4ff3a3781a63cb
ChoiAnYong <dksdyd78@naver.com>
2025-09-09 18:44:36 +0900

    refactor: #445-Delegate 사용

--- Spoony-iOS/Spoony-iOS/Source/Feature/Scene/MyPage/Setting/AccountManagement/AccountManagementFeature.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Scene/MyPage/Setting/AccountManagement/AccountManagementFeature.swift
@@ -41,16 +41,21 @@ struct AccountManagementFeature {
         case binding(BindingAction<State>)
         case onAppear
         case userInfoResponse(TaskResult<UserInfoResponse>)
-        case routeToPreviousScreen
         case selectLoginType(LoginType)
         case logoutButtonTapped
         case confirmLogout
         case cancelLogout
         case withdrawButtonTapped
-        case routeToWithdrawScreen
         case performLogout
         case logoutResult(TaskResult<Bool>)
-        case routeToLoginScreen
+        
+        // MARK: - Route Action: 화면 전환 이벤트를 상위 Reducer에 전달 시 사용
+        case delegate(Delegate)
+        enum Delegate {
+            case routeToWithdrawScreen
+            case routeToPreviousScreen
+            case routeToLoginScreen
+        }
     }
     
     @Dependency(\.authService) var authService: AuthProtocol
@@ -79,9 +84,6 @@ struct AccountManagementFeature {
                 print("Error fetching user info: \(error.localizedDescription)")
                 return .none
                 
-            case .routeToPreviousScreen:
-                return .none
-                
             case let .selectLoginType(type):
                 state.currentLoginType = type
                 return .none
@@ -99,10 +101,7 @@ struct AccountManagementFeature {
                 return .none
                 
             case .withdrawButtonTapped:
-                return .send(.routeToWithdrawScreen)
-                
-            case .routeToWithdrawScreen:
-                return .none
+                return .send(.delegate(.routeToWithdrawScreen))
                 
             case .performLogout:
                 state.isLoggingOut = true
@@ -130,7 +129,7 @@ struct AccountManagementFeature {
                     UserManager.shared.exploreUserRecentSearches = nil
                     UserManager.shared.exploreReviewRecentSearches = nil
                     
-                    return .send(.routeToLoginScreen)
+                    return .send(.delegate(.routeToLoginScreen))
                 } else {
                     state.logoutErrorMessage = "로그아웃에 실패했습니다."
                 }
@@ -142,7 +141,7 @@ struct AccountManagementFeature {
                 print("로그아웃 실패: \(error.localizedDescription)")
                 return .none
                 
-            case .routeToLoginScreen:
+            case .delegate:
                 return .none
             }
         }
