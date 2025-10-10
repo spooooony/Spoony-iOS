33efe9035f43e0aa1495146cec42f62d73a9da62
ChoiAnYong <dksdyd78@naver.com>
2025-10-10 17:42:47 +0900

    feat: #463 - RegisterInterface 구현

--- Spoony-iOS/Spoony-iOS/Source/Feature/Scene/Register/Domain/Interface/RegisterInterface.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Scene/Register/Domain/Interface/RegisterInterface.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+protocol RegisterInterface {
+    func searchPlace(query: String) async throws -> [PlaceInfoEntity]
+    func validatePlace(latitude: Double, longitude: Double) async throws -> Bool
+    func registerPost(
+        title: String,
+        description: String,
+        value: Double,
+        cons: String?,
+        placeName: String,
+        placeAddress: String,
+        placeRoadAddress: String,
+        latitude: Double,
+        longitude: Double,
+        categoryId: Int,
+        menuList: [String],
+        imagesData: [Data]
+    ) async throws -> Bool 
+    func editPost(
+        postId: Int,
+        description: String,
+        value: Double,
+        cons: String,
+        categoryId: Int,
+        menuList: [String],
+        deleteImageUrlList: [String],
+        imagesData: [Data]
+    ) async throws -> Bool
+    func getRegisterCategories() async throws -> [CategoryChipEntity]
+    func getReviewInfo(postId: Int) async throws -> ReviewInfoEntity
+}
