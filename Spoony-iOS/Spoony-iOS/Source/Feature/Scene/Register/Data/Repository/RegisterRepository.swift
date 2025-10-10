2843400863564f9c5787c07c28076cc00c4adc43
ChoiAnYong <dksdyd78@naver.com>
2025-10-10 17:43:51 +0900

    feat: #463 - RegisterRepository 구현

--- Spoony-iOS/Spoony-iOS/Source/Feature/Scene/Register/Data/Repository/RegisterRepository.swift
+++ Spoony-iOS/Spoony-iOS/Source/Feature/Scene/Register/Data/Repository/RegisterRepository.swift
@@ -6,3 +6,83 @@
 //
 
 import Foundation
+
+struct RegisterRepository: RegisterInterface {
+    private let service: RegisterServiceType
+    
+    init(service: RegisterServiceType) {
+        self.service = service
+    }
+    
+    func searchPlace(query: String) async throws -> [PlaceInfoEntity] {
+        try await service.searchPlace(query: query).toEntity()
+    }
+    
+    func validatePlace(latitude: Double, longitude: Double) async throws -> Bool {
+        let request = ValidatePlaceRequestDTO(latitude: latitude, longitude: longitude)
+        
+        return try await service.validatePlace(request: request).duplicate
+    }
+    
+    func registerPost(
+        title: String,
+        description: String,
+        value: Double,
+        cons: String?,
+        placeName: String,
+        placeAddress: String,
+        placeRoadAddress: String,
+        latitude: Double,
+        longitude: Double,
+        categoryId: Int,
+        menuList: [String],
+        imagesData: [Data]
+    ) async throws -> Bool {
+        let request = RegisterPostRequestDTO(
+            title: title,
+            description: description,
+            value: value,
+            cons: cons,
+            placeName: placeName,
+            placeAddress: placeAddress,
+            placeRoadAddress: placeRoadAddress,
+            latitude: latitude,
+            longitude: longitude,
+            categoryId: categoryId,
+            menuList: menuList
+        )
+        
+        return try await service.registerPost(request: request, imagesData: imagesData)
+    }
+    
+    func editPost(
+        postId: Int,
+        description: String,
+        value: Double,
+        cons: String,
+        categoryId: Int,
+        menuList: [String],
+        deleteImageUrlList: [String],
+        imagesData: [Data]
+    ) async throws -> Bool {
+        let request = EditPostRequestDTO(
+            postId: postId,
+            description: description,
+            value: value,
+            cons: cons,
+            categoryId: categoryId,
+            menuList: menuList,
+            deleteImageUrlList: deleteImageUrlList
+        )
+        
+        return try await service.editPost(request: request, imagesData: imagesData)
+    }
+    
+    func getRegisterCategories() async throws -> [CategoryChipEntity] {
+        try await service.getRegisterCategories().toEntity()
+    }
+    
+    func getReviewInfo(postId: Int) async throws -> ReviewInfoEntity {
+        try await service.getReviewInfo(postId: postId).toEntity()
+    }
+}
